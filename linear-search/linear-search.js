f2f494d6a71b1d65a48534d097a2d4062ab9989b
Alisa <alisakovalhome@mail.ru>
2023-06-01 12:00:00 +0300

    Add linear-search

--- linear-search/linear-search.js
+++ linear-search/linear-search.js
@@ -2,13 +2,14 @@
 //принимает два аргумента: объект и массив. Она должна последовательно перебирать каждый элемент и возвращать индекс,
 // в котором объект был изначально найден. Если объект не найден, то функция возвращает значение -1.
 function linearSearch(n, arr) {
-
+    return arr.indexOf(n)
 }
 
 
 //напишем новый метод globalLinearSearch, который возвращает массив всех индексов, в которых найден элемент.
 function globalLinearSearch(n, arr) {
-
+    return arr.map((el, index) => el === n ? index : '')
+        .filter((el) => Boolean(el))
 }
 
 module.exports = {
